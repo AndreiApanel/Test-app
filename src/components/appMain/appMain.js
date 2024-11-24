4b29237414f86dab2561c16e27bec9a7e8454a40
Andrei Apanel <aposha84@gmail.com>
2024-11-24 23:45:43 +0100

    One step: create main component

--- src/components/appMain/appMain.js
+++ src/components/appMain/appMain.js
@@ -1,1 +1,15 @@
-import '.appMain.scss';
+import './appMain.scss';
+import { Component } from 'react';
+
+class AppMain extends Component {
+	render() {
+		return (
+			<div className="app-main">
+				<h1>Everything You Love About Coffee</h1>
+				<p></p>
+			</div>
+		);
+	}
+}
+
+export default AppMain;
