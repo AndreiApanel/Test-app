6c3a42d1a1e6bd59ee344f6761d44c50f0b8adc4
Andrei Apanel <aposha84@gmail.com>
2024-12-23 00:04:57 +0100

    Created a main component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
-
 import App from './components/app/App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-	<React.StrictMode>
-		<App />
-	
-	</React.StrictMode>
-);
-
+const root = createRoot(document.getElementById('app'));
+root.render(<App />);
